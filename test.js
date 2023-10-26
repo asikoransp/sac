d12a529c1b6409ff42abb2acdaa074ff7a370e3c
Adrian Sikora <adrian.sikora@sovanta.com>
2023-10-26 09:09:50 +0200

    test: charts

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 (function () {
   let tmpl = document.createElement("template");
   tmpl.innerHTML = `
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <h1>TEST</h1>
     </br>
     <button type="button" id="myBtn">Helper Button</button>
+    </br>
+    <canvas id="myChart" width="500" height="400"></canvas>
     `;
 
   class PerformanceHelp extends HTMLElement {
@@ -19,6 +22,31 @@
         var event = new Event("onClick");
         this.fireChanged();
         this.dispatchEvent(event);
+      });
+
+      let labels1 = ["YES", "YES BUT IN GREEN"];
+      let data1 = [69, 31];
+      let colors1 = ["#49A9EA", "#36CAAB"];
+
+      let myDoughnutChart = document.getElementById("myChart").getContext("2d");
+
+      let chart1 = new Chart(myDoughnutChart, {
+        type: "doughnut",
+        data: {
+          labels: ["YES", "YES BUT IN GREEN"],
+          datasets: [
+            {
+              data: [69, 31],
+              backgroundColor: ["#49A9EA", "#36CAAB"],
+            },
+          ],
+        },
+        options: {
+          title: {
+            text: "Do you like doughnuts?",
+            display: true,
+          },
+        },
       });
     }
 
