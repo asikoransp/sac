1dfed5e8033f7386693bb8649e5b41d644c4bd50
Adrian Sikora <adrian.sikora@sovanta.com>
2023-10-26 08:45:42 +0200

    test

--- test.js
+++ test.js
@@ -1,21 +1,27 @@
 (function () {
-  let template = document.createElement("template");
-  template.innerHTML = `
-      <style>
-        .name {
-            font-family: Arial, sans-serif;
-            color:green;
-        }
-        </style>
-        <h1 class="name">Welcome to test</h1>
-      `;
-  class testWidget extends HTMLElement {
+  let tmpl = document.createElement("template");
+  tmpl.innerHTML = `<button type="button" id="myBtn">Helper Button</button>`;
+
+  class PerformanceHelp extends HTMLElement {
     constructor() {
       super();
       this.init();
     }
 
-    async init() {}
+    init() {
+      let shadowRoot = this.attachShadow({ mode: "open" });
+      shadowRoot.appendChild(tmpl.content.cloneNode(true));
+      this.addEventListener("click", (event) => {
+        var event = new Event("onClick");
+        this.fireChanged();
+        this.dispatchEvent(event);
+      });
+    }
+
+    fireChanged() {
+      console.log("OnClick Triggered");
+    }
   }
-  customElements.define("test-widget", testWidget);
+
+  customElements.define("custom-button", PerformanceHelp);
 })();
