5b4c863028e520a397b6ef03c9cf4791224dde70
Adrian Sikora <adrian.sikora@sovanta.com>
2023-11-23 12:07:33 +0100

    proxy

--- topTenProducts/index.js
+++ topTenProducts/index.js
@@ -12074,16 +12074,28 @@
       if (this.chart) return;
 
       if (this.dataSet && this.dataSet.data) {
-        const handler = {
-          set(target, prop, value) {
-            console.log(`changed ${prop} from ${target[prop]} to ${value}`);
-            target[prop] = value;
-          },
+        var handler = function () {
+          return {
+            get: function (obj, prop) {
+              console.log("got it!");
+              if (
+                ["[object Object]", "[object Array]"].indexOf(
+                  Object.prototype.toString.call(obj[prop])
+                ) > -1
+              ) {
+                return new Proxy(obj[prop], handler());
+              }
+              return obj[prop];
+            },
+            set: function (obj, prop, value) {
+              console.log("set it");
+              obj[prop] = value;
+              return true;
+            },
+          };
         };
 
-        this.dataSet.data = new Proxy(this.dataSet.data, handler);
-
-        console.log(this.dataSet.data);
+        this.dataSet = new Proxy(this.dataSet, handler());
 
         const data = this.getData();
 
