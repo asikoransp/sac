e59cddedc715f0a82f5bcb7f33688dd364717158
Daniel Budzynski <daniel.budzynski@nsp.biz.pl>
2023-12-08 10:44:12 +0100

    jgjyghj

--- companySalesRevenue/index.js
+++ companySalesRevenue/index.js
@@ -12172,7 +12172,7 @@
               y2: {
                 position: 'right',
                 min: -100,
-                max: 20000,
+                max: 2000,
                 grid: {
                   display: false
                 }
