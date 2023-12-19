c699cb18bd706884b099bdb6a82d4893befbcc6c
Adrian Sikora <adrian.sikora@sovanta.com>
2023-12-19 13:02:15 +0100

    test babel

--- topTenProducts/test.js
+++ topTenProducts/test.js
@@ -1,3 +1,9 @@
-export const test = (text) => {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.test = void 0;
+var test = exports.test = function test(text) {
   console.log(text);
 };
