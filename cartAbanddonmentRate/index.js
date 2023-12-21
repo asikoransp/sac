7d2fc774ffa56dfc5902240c513bb4ae64e64d9b
Daniel Budzynski <daniel.budzynski@nsp.biz.pl>
2023-12-21 14:14:34 +0100

    eaew

--- cartAbanddonmentRate/index.js
+++ cartAbanddonmentRate/index.js
@@ -12125,32 +12125,32 @@
             // }
           },
         });
-        this.textCenter("tst");
+        // this.textCenter("tst");
 
 
       }
     }
-    textCenter(val) {
-      Chart.pluginService.register({
-        beforeDraw: function(chart) {
-          var width = chart.chart.width,
-              height = chart.chart.height,
-              ctx = chart.chart.ctx;
+    // textCenter(val) {
+    //   Chart.pluginService.register({
+    //     beforeDraw: function(chart) {
+    //       var width = chart.chart.width,
+    //           height = chart.chart.height,
+    //           ctx = chart.chart.ctx;
     
-          ctx.restore();
-          var fontSize = (height / 114).toFixed(2);
-          ctx.font = fontSize + "em sans-serif";
-          ctx.textBaseline = "middle";
+    //       ctx.restore();
+    //       var fontSize = (height / 114).toFixed(2);
+    //       ctx.font = fontSize + "em sans-serif";
+    //       ctx.textBaseline = "middle";
     
-          var text = val+"%",
-              textX = Math.round((width - ctx.measureText(text).width) / 2),
-              textY = height / 2;
+    //       var text = val+"%",
+    //           textX = Math.round((width - ctx.measureText(text).width) / 2),
+    //           textY = height / 2;
     
-          ctx.fillText(text, textX, textY);
-          ctx.save();
-        }
-      });
-    }
+    //       ctx.fillText(text, textX, textY);
+    //       ctx.save();
+    //     }
+    //   });
+    // }
   }
 
   customElements.define("cart-abanddonment-rate", PerformanceHelp);
