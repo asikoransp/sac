92511c22e2c4ddea793d5a49d308e4b90b6d633e
Daniel Budzynski <daniel.budzynski@nsp.biz.pl>
2023-12-21 15:13:54 +0100

    sdf

--- cartAbanddonmentRate/index.js
+++ cartAbanddonmentRate/index.js
@@ -12014,7 +12014,22 @@
                     pink: "rgba(255, 70, 118, 0.5)",
                     blue: "rgba(0, 82, 255, 0.6)"
                   };
-
+                  
+                  myPlugin = {
+                    id: 'myPlugin',
+                    beforeDraw: (chart) => {
+                      const ctx = chart.ctx;
+                      const xCoor = chart.chartArea.left + (chart.chartArea.right - chart.chartArea.left) / 2;
+                      const yCoor = chart.chartArea.top + (chart.chartArea.bottom - chart.chartArea.top) / 2;
+                      ctx.save();
+                      ctx.font = 'bolder 24px';
+                      ctx.fillStyle = 'red';
+                      ctx.textAlign = 'center';
+                      ctx.textBaseline = 'middle';
+                      ctx.fillText(`${input1}`, xCoor, yCoor);
+                      ctx.restore();
+                    },
+                }
     constructor() {
       super();
       this.init();
@@ -12065,21 +12080,7 @@
       if (this.chart) return;
       if (this.dataSet && this.dataSet.data) {
         const data = this.getData();
-        const myPlugin = {
-          id: 'myPlugin',
-          beforeDraw: (chart) => {
-            const ctx = chart.ctx;
-            const xCoor = chart.chartArea.left + (chart.chartArea.right - chart.chartArea.left) / 2;
-            const yCoor = chart.chartArea.top + (chart.chartArea.bottom - chart.chartArea.top) / 2;
-            ctx.save();
-            ctx.font = 'bolder 24px';
-            ctx.fillStyle = 'red';
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
-            ctx.fillText(`${input1}`, xCoor, yCoor);
-            ctx.restore();
-          },
-      }
+
         const chartElement = this.template
           .querySelector("canvas")
           .getContext("2d");
@@ -12105,7 +12106,7 @@
               legend: {
                 position: 'top',
               },
-              myPlugin
+              this.myPlugin
             },
           },
         });
