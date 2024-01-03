8377792178c08ece3338526ba4762a8432ee52c0
Adrian Sikora <adrian.sikora@sovanta.com>
2024-01-03 12:42:39 +0100

    11

--- topTenProducts/index.js
+++ topTenProducts/index.js
@@ -12211,22 +12211,22 @@
         ) {
           chartWrapper.setAttribute(
             "style",
-            "transition-delay: 1s; transition: all 1s ease-out; height: 370px !important;"
+            "transition-delay: 2s; transition-property: height; transition: all 1s ease-out; height: 370px !important;"
           );
           // widgetWrapper.setAttribute("style", "width: 100%;");
           widget.setAttribute(
             "style",
-            "transition-delay: 1s; transition: all 1s ease-out; display: block;"
+            "transition-delay: 2s; transition-property: display; transition: all 1s ease-out; display: block;"
           );
         } else {
           chartWrapper.setAttribute(
             "style",
-            "transition-delay: 1s; transition: all 1s ease-out; height: 800px !important;"
+            "transition-delay: 2s; transition: all 1s ease-out; transition-property: height; height: 800px !important;"
           );
           // widgetWrapper.setAttribute("style", "width: 90%;");
           widget.setAttribute(
             "style",
-            "transition-delay: 1s; transition: all 1s ease-out; display: flex; align-items: center; justify-content: center;"
+            "transition-delay: 2s; transition: all 1s ease-out; transition-property: display; display: flex; align-items: center; justify-content: center;"
           );
         }
       });
