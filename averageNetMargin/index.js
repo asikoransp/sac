f67ac2c2e115a71234b4330e7e092398c87aef0c
Adrian Sikora <adrian.sikora@sovanta.com>
2024-01-04 13:45:19 +0100

    avg net margin

---
+++ averageNetMargin/index.js
@@ -0,0 +1,249 @@
+(function () {
+  const svgButtons = `
+    <button class="full-screen__btn" id="full-screen-mode-btn">
+      <svg id="expand-btn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+      </svg>
+      <svg id="collapse-btn" class="none-display" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 9V4.5M9 9H4.5M9 9 3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5 5.25 5.25" />
+      </svg>
+    </button>
+  `;
+
+  let tmpl = document.createElement("template");
+  tmpl.innerHTML = `
+    <div class="widget__wrapper" style="display: none;">
+      <div class="widget__header">
+        <h2 class="widget__title">Average Net Margin</h2>
+        ${svgButtons}
+      </div>
+      <div class="chart__wrapper">
+        <canvas id="average-rebate-rate-chart"></canvas>
+      </div>
+    </div>
+  `;
+
+  // grid opacity 0.5 for blending
+  // background opacity 1 for filtering
+  // chart opacity 0.8 for blending
+
+  class PerformanceHelp extends HTMLElement {
+    template = undefined;
+    chart = undefined;
+    currentColor = undefined;
+    colors = {
+      lightMode: {
+        background: "rgba(230, 230, 230, 1)",
+        text: "rgba(70, 70, 70, 1)",
+        grid: "rgba(130, 130, 130, 0.5)",
+        chart: {
+          primary: "rgba(70, 49, 238, 0.8)",
+          secondary: "rgba(255, 70, 118, 0.8)",
+        },
+      },
+      darkMode: {
+        background: "rgba(70, 70, 70, 1)",
+        text: "rgba(220, 220, 220, 1)",
+        grid: "rgba(210, 210, 210, 0.5)",
+        chart: {
+          primary: "rgba(210, 210, 210, 0.8)",
+          secondary: "rgba(255, 70, 118, 0.8)",
+        },
+      },
+    };
+
+    constructor() {
+      super();
+      this.init();
+    }
+
+    init() {
+      let shadowRoot = this.attachShadow({ mode: "open" });
+      shadowRoot.appendChild(tmpl.content.cloneNode(true));
+      this.template = shadowRoot;
+    }
+
+    onCustomWidgetBeforeUpdate(changedProperties) {
+      if (changedProperties) {
+        if (changedProperties.lightModeChartColor)
+          this.colors.lightMode.chart.primary =
+            changedProperties.lightModeChartColor;
+
+        if (changedProperties.darkModeChartColor)
+          this.colors.darkMode.chart.primary =
+            changedProperties.darkModeChartColor;
+
+        if (changedProperties.chartTitle)
+          this.template.querySelector(".widget__title").innerText =
+            changedProperties.chartTitle;
+      }
+
+      const element = document.querySelector(
+        ".sap-user-defined-dark-mode-theme"
+      );
+
+      this.currentColor = element
+        ? this.colors.darkMode
+        : this.colors.lightMode;
+
+      this.adjustStyles();
+      this.addGlobalStyles();
+    }
+
+    onCustomWidgetAfterUpdate(changedProperties) {
+      if (!this.dataSet || !this.dataSet.data) return;
+
+      if (this.chart) {
+        this.updateChartData();
+        return;
+      }
+
+      this.renderChart();
+    }
+
+    updateChartData() {
+      const data = this.getData();
+      this.chart.data.datasets[0].data = data.values;
+      this.chart.data.labels = data.labels;
+      this.chart.update();
+    }
+
+    getData() {
+      console.log(this.dataSet.data);
+
+      const dataSet = this.dataSet.data
+        .sort((a, b) => b.measures_0.raw - a.measures_0.raw)
+        .slice(0, 10);
+
+      const labels = dataSet.map((el) =>
+        el.dimensions_0.label.split("_").join(" ")
+      );
+      const values = dataSet.map((el) => el.measures_0.raw);
+
+      return {
+        labels,
+        values,
+      };
+    }
+
+    renderChart() {
+      const data = this.getData();
+
+      const chartElement = this.template
+        .querySelector("canvas")
+        .getContext("2d");
+
+      this.chart = new Chart(chartElement, {
+        type: "bar",
+        data: {
+          labels: data.labels,
+          datasets: [
+            {
+              label: "Value",
+              data: data.values,
+              backgroundColor: this.currentColor.chart.primary,
+              borderWidth: 0,
+              borderColor: this.currentColor.chart.primary,
+              borderRadius: 5,
+              borderSkipped: false,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          indexAxis: "x",
+          scales: {
+            x: {
+              grid: {
+                color: this.currentColor.grid,
+              },
+              ticks: {
+                color: this.currentColor.text,
+              },
+            },
+            y: {
+              grid: {
+                color: this.currentColor.grid,
+              },
+              ticks: {
+                color: this.currentColor.text,
+              },
+            },
+          },
+          plugins: {
+            title: {
+              display: false,
+            },
+            legend: {
+              display: false,
+            },
+          },
+        },
+      });
+
+      this.fullScreenModeHandler();
+    }
+
+    adjustStyles() {
+      this.template.querySelector(".widget__wrapper").style.background =
+        this.currentColor.background;
+      this.template.querySelector(".widget__title").style.color =
+        this.currentColor.text;
+      this.template.querySelector(".full-screen__btn").style.color =
+        this.currentColor.text;
+    }
+
+    addGlobalStyles() {
+      if (!this.template.querySelector("style"))
+        try {
+          const styles = document.querySelector('style[data-custom="true"]');
+          const shadowStyleEl = document.createElement("style");
+          shadowStyleEl.innerText = styles.innerText;
+          this.template.appendChild(shadowStyleEl);
+          this.template.querySelector(".widget__wrapper").style.display =
+            "block";
+        } catch (e) {}
+    }
+
+    fullScreenModeHandler() {
+      const fullScreenModeBtn = this.template.getElementById(
+        "full-screen-mode-btn"
+      );
+
+      document.addEventListener("fullscreenchange", () => {
+        const widget = document.querySelector("average-net-margin");
+        const widgetDOM = widget.shadowRoot;
+        const chartWrapper = widgetDOM.querySelector(".chart__wrapper");
+        const widgetWrapper = widgetDOM.querySelector(".widget__wrapper");
+        const expandBtn = widgetDOM.getElementById("expand-btn");
+        const collapseBtn = widgetDOM.getElementById("collapse-btn");
+
+        chartWrapper.classList.toggle("chart__wrapper--full-screen");
+        widgetWrapper.classList.toggle("widget__wrapper--full-screen");
+        expandBtn.classList.toggle("none-display");
+        collapseBtn.classList.toggle("none-display");
+      });
+
+      fullScreenModeBtn.addEventListener("click", () => {
+        const widget = document.querySelector("average-net-margin");
+        if (this.isNotFullScreenMode()) {
+          widget.requestFullscreen();
+        } else {
+          document.exitFullscreen();
+        }
+      });
+    }
+
+    isNotFullScreenMode() {
+      return (
+        !document.fullscreenElement &&
+        !document.mozFullScreenElement &&
+        !document.webkitFullscreenElement &&
+        !document.msFullscreenElement
+      );
+    }
+  }
+
+  customElements.define("average-net-margin", PerformanceHelp);
+})();
---
+++ averageNetMargin/styling.js
@@ -0,0 +1,128 @@
+(function () {
+  let template = document.createElement("template");
+  template.innerHTML = `
+    <style>
+      .styling-property {
+        margin-bottom: 0.5rem;
+        color: #979797;
+        font-size: 0.9rem;
+      }
+
+      .styling-property input[type="color"] {
+        border: none;
+        background: none;
+        width: 1.7rem;
+        height: 1.7rem;
+      }
+
+      .styling-property input[type="text"] {
+        width: 95%;
+        padding: 0.3rem 0.5rem;
+        border: 1px solid #c2c2c2;
+        color: #2c2c2c
+      }
+
+      .styling-property legend { 
+        margin-bottom: 0.3rem;
+        color: #979797;
+        font-size: 0.9rem;
+      }
+    </style>
+
+    <div class="styling-property">
+      <legend>Light mode</legend>
+      <input id="light-mode-chart-color" type="color">
+    </div>
+
+    <div class="styling-property">
+      <legend>Dark mode</legend>
+      <input id="dark-mode-chart-color" type="color">
+    </div>
+
+    <div class="styling-property">
+      <legend>Chart title</legend>
+      <input id="chart-title" type="text">
+    </div>
+	`;
+
+  class BoxSps extends HTMLElement {
+    constructor() {
+      super();
+      this._shadowRoot = this.attachShadow({ mode: "open" });
+      this._shadowRoot.appendChild(template.content.cloneNode(true));
+      this._shadowRoot.querySelectorAll("input").forEach((property) => {
+        property.addEventListener("change", this._submit.bind(this));
+      });
+    }
+
+    convertHexToRGBA(hexColor) {
+      if (hexColor.startsWith("#")) {
+        hexColor = hexColor.slice(1);
+      }
+
+      const red = parseInt(hexColor.substring(0, 2), 16);
+      const green = parseInt(hexColor.substring(2, 4), 16);
+      const blue = parseInt(hexColor.substring(4, 6), 16);
+
+      return `rgba(${red}, ${green}, ${blue}, 0.8)`;
+    }
+
+    convertRGBAToHex(rgbaColor) {
+      const rgbaValues = rgbaColor.match(/\d+/g);
+      const red = parseInt(rgbaValues[0]);
+      const green = parseInt(rgbaValues[1]);
+      const blue = parseInt(rgbaValues[2]);
+
+      return `#${red.toString(16).padStart(2, "0")}${green
+        .toString(16)
+        .padStart(2, "0")}${blue.toString(16).padStart(2, "0")}`;
+    }
+
+    _submit(e) {
+      e.preventDefault();
+      this.dispatchEvent(
+        new CustomEvent("propertiesChanged", {
+          detail: {
+            properties: {
+              lightModeChartColor: this.convertHexToRGBA(
+                this.lightModeChartColor
+              ),
+              darkModeChartColor: this.convertHexToRGBA(
+                this.darkModeChartColor
+              ),
+              chartTitle: this.chartTitle,
+            },
+          },
+        })
+      );
+    }
+
+    set lightModeChartColor(newLightModeChartColor) {
+      this._shadowRoot.getElementById("light-mode-chart-color").value =
+        this.convertRGBAToHex(newLightModeChartColor);
+    }
+
+    get lightModeChartColor() {
+      return this._shadowRoot.getElementById("light-mode-chart-color").value;
+    }
+
+    set darkModeChartColor(newDarkModeChartColor) {
+      this._shadowRoot.getElementById("dark-mode-chart-color").value =
+        this.convertRGBAToHex(newDarkModeChartColor);
+    }
+
+    get darkModeChartColor() {
+      return this._shadowRoot.getElementById("dark-mode-chart-color").value;
+    }
+
+    set chartTitle(newChartTitle) {
+      this._shadowRoot.getElementById("chart-title").value = newChartTitle;
+    }
+
+    get chartTitle() {
+      return this._shadowRoot.getElementById("chart-title").value;
+    }
+  }
+
+  customElements.define("average-net-margin-sps", BoxSps);
+})();
